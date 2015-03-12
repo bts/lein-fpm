f25d280a334c64bb218d7e5c9a87a9c74b1ccc4d
Brian Schroeder <bts@gmail.com>
2015-03-12 11:57:10 -0400

    Update description in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-fpm "0.2.3"
-  :description "A Leiningen plugin for generating simple packages using fpm."
+  :description "A Leiningen plugin for building simple packages using fpm."
   :url "http://github.com/bts/lein-fpm"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
